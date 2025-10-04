cf21db7d3a044a8483f16af5768073101041786d
vojtech.zmolik <anonymous.escaper@gmail.com>
2025-10-04 16:37:33 +0200

    smazat poak

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Localization;
 using Serilog;
+using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +15,9 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddHttpClient("couchdb");
 builder.Services.AddHttpClient<WebApplication1.Service.CouchDbService>();
 builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+builder.Services
+    .AddViewLocallization()
+    .AddDataAnnotationsLocalization();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,6 +27,14 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+var supportedCultures = new[] { new CultureInfo("cs"), new CultureInfo("en") };
+
+app.UseRequestLocalization(new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture("cs"),
+    SupportedCultures = supportedCultures,
+    SupportedUICultures = supportedCultures
+});
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
